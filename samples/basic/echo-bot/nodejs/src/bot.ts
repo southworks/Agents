55f81e2711ab548b588e9875deecab2ce8b27a76
Jhonatan Sandoval Velasco <122501764+JhontSouth@users.noreply.github.com>
2025-02-19 23:43:49 -0500

    Add node echo bot sample (#66)
    * add node echo bot sample
    
    * move to right folder
    
    * Refactor echo bot sample: add new structure, update configuration, and remove deprecated files
    
    * Update Echo bot README and welcome message to include SDK version
    
    * Remove unnecessary npm scripts from echo bot package.json
    
    * Rename package to "echo-bot" and remove unnecessary whitespace in index.ts
    
    * Add CI workflow for Node.js builds in GitHub Actions
    
    * Update CI workflow to build echo bot from specific directory
    
    ---------
    
    Co-authored-by: rido <rmpablos@microsoft.com>

---
+++ samples/basic/echo-bot/nodejs/src/bot.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { ActivityHandler, MessageFactory } from '@microsoft/agents-bot-hosting'
+import { version } from '@microsoft/agents-bot-hosting/package.json'
+
+export class EchoBot extends ActivityHandler {
+  constructor() {
+    super()
+    this.onMessage(async (context, next) => {
+      const replyText = `Echo: ${context.activity.text}`
+      await context.sendActivity(MessageFactory.text(replyText, replyText))
+      await next()
+    })
+
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded ?? []
+      const welcomeText = '🚀 Echo bot running on Agents SDK version: ' + version
+      for (const member of membersAdded) {
+        if (member.id !== (context.activity.recipient?.id ?? '')) {
+          await context.sendActivity(MessageFactory.text(welcomeText, welcomeText))
+        }
+      }
+      await next()
+    })
+  }
+}
