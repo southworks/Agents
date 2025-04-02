42e56475ae0872e94702756e7a81845dd015207e
Jhonatan Sandoval Velasco <122501764+JhontSouth@users.noreply.github.com>
2025-04-02 17:51:08 -0500

    Use AgentApp in Echo sample (#96)
    * include application-style-sample
    
    * update agents packages references
    
    * Replaced "bot" with "agent" where appropriate
    
    * Add LangChain sample. Update node packages (#95)
    
    * Add WeatherBot sample with Node.js implementation and necessary configurations
    
    * Refactor WeatherBot code for improved readability and consistency
    
    * Update WeatherBot sample to use new Microsoft package registry and dependencies
    
    * Clean up whitespace in myAgent.ts for improved code clarity
    
    * Rename Node.js package and add Dockerfile for deployment
    
    * Update WeatherBot sample to use new Microsoft package registry and update dependencies
    
    * Simplify express import statement in index.ts for cleaner code
    
    * Remove .npmrc file and update package.json dependency version; add README for WeatherAgent sample
    
    * update mcs client to new packages
    
    * Update package.json and import statements to use new @microsoft/agents-hosting package
    
    * Remove deprecated application-style sample files and update dependencies
    
    ---------
    
    Co-authored-by: Tracy Boehrer <trboehre@microsoft.com>
    Co-authored-by: Rido <rido-min@users.noreply.github.com>
    Co-authored-by: MattB <mattb-msft@hotmail.com>

--- samples/basic/echo-bot/nodejs/src/index.ts
+++ samples/basic/echo-bot/nodejs/src/index.ts
@@ -1,25 +1,23 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
+import { AuthConfiguration, authorizeJWT, CloudAdapter, loadAuthConfigFromEnv, Request } from '@microsoft/agents-hosting'
 import express, { Response } from 'express'
-import { Request, CloudAdapter, authorizeJWT, AuthConfiguration, loadAuthConfigFromEnv } from '@microsoft/agents-bot-hosting'
-import { EchoBot } from './bot'
+import { agentApp } from './agent'
 
 const authConfig: AuthConfiguration = loadAuthConfigFromEnv()
-
 const adapter = new CloudAdapter(authConfig)
-const myBot = new EchoBot()
 
-const app = express()
-app.use(express.json())
-app.use(authorizeJWT(authConfig))
+const server = express()
+server.use(express.json())
+server.use(authorizeJWT(authConfig))
 
-app.post('/api/messages', async (req: Request, res: Response) => {
-  await adapter.process(req, res, async (context) => await myBot.run(context))
+server.post('/api/messages', async (req: Request, res: Response) => {
+  await adapter.process(req, res, async (context) => {
+    const app = agentApp
+    await app.run(context)
+  })
 })
 
 const port = process.env.PORT || 3978
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`)
 }).on('error', (err) => {
   console.error(err)
