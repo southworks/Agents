730cb20a768d9d379336e50976381d0abd231e1f
Rido <rido-min@users.noreply.github.com>
2025-05-27 14:44:27 -0700

    Fix askQuestion logic to handle empty responses correctly (#170)
    * Fix askQuestion logic to handle empty responses correctly
    
    * Fix askQuestion function to return early on 'exit' command

--- samples/basic/copilotstudio-client/nodejs/src/index.ts
+++ samples/basic/copilotstudio-client/nodejs/src/index.ts
@@ -78,7 +78,8 @@ const askQuestion = async (copilotClient: CopilotStudioClient, conversationId: s
   rl.question('\n>>>: ', async (answer) => {
     if (answer.toLowerCase() === 'exit') {
       rl.close()
-    } else {
+      return
+    } else if (answer.length > 0){
       const replies = await copilotClient.askQuestionAsync(answer, conversationId)
       replies.forEach((act: Activity) => {
         if (act.type === ActivityTypes.Message) {
@@ -89,8 +90,8 @@ const askQuestion = async (copilotClient: CopilotStudioClient, conversationId: s
           rl.close()
         }
       })
-      await askQuestion(copilotClient, conversationId)
     }
+    await askQuestion(copilotClient, conversationId)
   })
 }
 
