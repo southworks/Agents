60d620f796edb5982fe1ebac1a876f654ae39b0b
CeciliaAvila <cecilia.avila@southworks.com>
2025-07-23 13:28:26 -0300

    Update settings.js file in webclient sample

--- samples/basic/copilotstudio-webclient/web/settings.TEMPLATE.js
+++ samples/basic/copilotstudio-webclient/web/settings.TEMPLATE.js
@@ -3,27 +3,33 @@
  * Licensed under the MIT License.
  */
 
+// This file emulates the process object in node.
+// rename this file to settings.js before running this test sample
+import { ConnectionSettings } from '@microsoft/agents-copilotstudio-client'
+
 // Flag to enable debug mode, which will store the debug information in localStorage.
 // Copilot Studio Client uses the "debug" library for logging (https://github.com/debug-js/debug?tab=readme-ov-file#browser-support).
 window.localStorage.debug = 'copilot-studio-client'
 
-export const settings = {
+export const settings = new ConnectionSettings({
   // App ID of the App Registration used to log in, this should be in the same tenant as the Copilot.
   appClientId: '',
   // Tenant ID of the App Registration used to log in, this should be in the same tenant as the Copilot.
   tenantId: '',
+  // Authority endpoint for the Azure AD login. Default is 'https://login.microsoftonline.com'.
+  authority: '',
   // Environment ID of the environment with the Copilot Studio App.
   environmentId: '',
   // Schema Name of the Copilot to use.
   agentIdentifier: '',
   // PowerPlatformCloud enum key.
-  cloud: '',
+  cloud: undefined,
   // Power Platform API endpoint to use if Cloud is configured as "Other".
-  customPowerPlatformCloud: '',
+  customPowerPlatformCloud: undefined,
   // AgentType enum key.
-  copilotAgentType: '',
+  copilotAgentType: undefined,
   // URL used to connect to the Copilot Studio service.
-  // directConnectUrl: 'https://dbe9da0559b6ea34b8c95474ae0402.16.environment.api.powerplatform.com/copilotstudio/dataverse-backed/authenticated/bots/cr761_agentsSdkDev/conversations?api-version=2022-03-01-preview',
+  directConnectUrl: undefined,
   // Flag to use the "x-ms-d2e-experimental" header URL on subsequent calls to the Copilot Studio service.
-  useExperimentalEndpoint: false,
-}
+  useExperimentalEndpoint: false
+})
