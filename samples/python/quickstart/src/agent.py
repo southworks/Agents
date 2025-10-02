39a23879c5f313d8e4de04389fbb01e82d8195c1
Thomas McLaughlin <90363044+mclaughlin-thomas@users.noreply.github.com>
2025-10-01 20:12:34 -0400

    Refactor agent.py: remove redundant comments
    Removes comments that are not indicative of what the code is doing.

--- samples/python/quickstart/src/agent.py
+++ samples/python/quickstart/src/agent.py
@@ -19,7 +19,7 @@ from microsoft_agents.hosting.core import (
 from microsoft_agents.authentication.msal import MsalConnectionManager
 from microsoft_agents.activity import load_configuration_from_env
 
-load_dotenv()  # robrandao: todo
+load_dotenv()
 agents_sdk_config = load_configuration_from_env(environ)
 
 STORAGE = MemoryStorage()
@@ -27,7 +27,7 @@ CONNECTION_MANAGER = MsalConnectionManager(**agents_sdk_config)
 ADAPTER = CloudAdapter(connection_manager=CONNECTION_MANAGER)
 AUTHORIZATION = Authorization(STORAGE, CONNECTION_MANAGER, **agents_sdk_config)
 
-# robrandao: downloader?
+
 AGENT_APP = AgentApplication[TurnState](
     storage=STORAGE, adapter=ADAPTER, authorization=AUTHORIZATION, **agents_sdk_config
 )
