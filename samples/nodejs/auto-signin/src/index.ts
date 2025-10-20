a54a6097344dec20326427ea05e389c5eb933958
CeciliaAvila <cecilia.avila@southworks.com>
2025-10-20 16:51:14 -0300

    Update nodejs auto-signin sample

--- samples/nodejs/auto-signin/src/index.ts
+++ samples/nodejs/auto-signin/src/index.ts
@@ -73,34 +73,31 @@ class AutoSignInDemo extends AgentApplication<TurnState> {
 
   private _pullRequests = async (context: TurnContext, state: TurnState): Promise<void> => {
     const userTokenResponse = await this.authorization.getToken(context, 'github')
-    if (userTokenResponse && userTokenResponse.token) {
-      const ghProf = await getCurrentProfile(userTokenResponse.token)
-      // console.log('GitHub profile', ghProf)
+    if (!userTokenResponse.token) {
+      await context.sendActivity(MessageFactory.text('Token not available. Please sign in with GitHub.'))
+      return
+    }
 
-      const userTemplate = (await import('./userProfileCard.json', { with: { type: 'json' } })).default
-      const template = new Template(userTemplate)
-      const card = template.expand(ghProf)
-      const activity = MessageFactory.attachment(CardFactory.adaptiveCard(card))
-      await context.sendActivity(activity)
+    const ghProf = await getCurrentProfile(userTokenResponse.token)
+    const userTemplate = (await import('./userProfileCard.json', { with: { type: 'json' } })).default
+    const template = new Template(userTemplate)
+    const card = template.expand(ghProf)
+    const activity = MessageFactory.attachment(CardFactory.adaptiveCard(card))
+    await context.sendActivity(activity)
 
-      const prs = await getPullRequests('microsoft', 'agents', userTokenResponse.token)
-      for (const pr of prs) {
-        const prCard = (await import('./pullRequestCard.json', { with: { type: 'json' } })).default
-        const template = new Template(prCard)
-        const toExpand = {
-          $root: {
-            title: pr.title,
-            url: pr.url,
-            id: pr.id,
-          }
+    const prs = await getPullRequests('microsoft', 'agents', userTokenResponse.token)
+    for (const pr of prs) {
+      const prCard = (await import('./pullRequestCard.json', { with: { type: 'json' } })).default
+      const template = new Template(prCard)
+      const toExpand = {
+        $root: {
+          title: pr.title,
+          url: pr.url,
+          id: pr.id,
         }
-        const card = template.expand(toExpand)
-        await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(card)))
       }
-    } else {
-      const tokenResponse = await this.authorization.beginOrContinueFlow(context, state, 'github')
-      console.warn(`GitHub token: ${JSON.stringify(tokenResponse)}`)
-      await context.sendActivity(MessageFactory.text('GitHub token length.' + tokenResponse?.token?.length))
+      const card = template.expand(toExpand)
+      await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(card)))
     }
   }
 }
