999ce39fe857d7fd03d24bd6fa4ad23f6156c222
CeciliaAvila <cecilia.avila@southworks.com>
2026-01-07 10:43:47 -0300

    Fix build error by creating settings.js when non existing

---
+++ samples/nodejs/copilotstudio-webchat-react/scripts/ensure-settings.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+
+// Ensures the settings.js file exists before building the project to avoid 
+// module not found errors.
+const fs = require('fs');
+const path = require('path');
+
+const src = path.resolve('./src/settings.EXAMPLE.js');
+const settings = path.resolve('./src/settings.js');
+
+if (!fs.existsSync(settings)) {
+  if (!fs.existsSync(src)) {
+    console.error('settings.EXAMPLE.js not found.');
+    process.exit(1);
+  }
+  fs.copyFileSync(src, settings);
+  console.log('Created settings.js from settings.EXAMPLE.js. Please update your configuration.');
+}
